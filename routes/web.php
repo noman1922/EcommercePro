089df41e66a58bebf6f6d92438c5a7edbb94705a
Abdullah Al Noman Khan <mdnomanahamed22@gmail.com>
2025-10-05 06:39:32 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\HomeController;
+
+use App\Http\Controllers\AdminController;
+
+route::get('/', [HomeController::class, 'index']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+route::get('/redirect', [HomeController::class, 'redirect']);
+
+route::get('/view_catagory', [AdminController::class, 'view_catagory']);
+route::post('/add_catagory', [AdminController::class, 'add_catagory']);
